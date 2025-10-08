5861fb844668ecfe6b59e5e8b91aff441c073bd0
amletodellaloggia <amletodellaloggia@gmail.com>
2025-10-08 12:46:52 +0200

    🌈 style:navbar navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "../styles/Navbar.css";
 import nerdNestLogo from "../assets/imgs/nerdNest-logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,27 +8,44 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark border-body">
     <div className="container-fluid">
-      <a className="navbar-brand" href="/">
+      <NavLink className="navbar-brand" to="/">
         <img src={nerdNestLogo} alt="NerdNest Logo" className="navbar-logo" />
         <span className="brand-text desktop">.nerdNest</span>
         <span className="brand-text mobile">.n</span>
-      </a>
+      </NavLink>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link fw-semibold" href="/">
+            <NavLink
+              className={({ isActive }) =>
+                "nav-link" +
+                (isActive ? " nav-active fw-bold" : " fw-semibold")
+              }
+              to="/"
+              end
+            >
               Home
-            </a>
+            </NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/products">
+            <NavLink
+              className={({ isActive }) =>
+                "nav-link" + (isActive ? " nav-active fw-bold" : "")
+              }
+              to="/products"
+            >
               Products
-            </a>
+            </NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/cart">
+            <NavLink
+              className={({ isActive }) =>
+                "nav-link" + (isActive ? " nav-active fw-bold" : "")
+              }
+              to="/cart"
+            >
               Cart
-            </a>
+            </NavLink>
           </li>
         </ul>
       </div>
