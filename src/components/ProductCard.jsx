abfdb8ef512b67eaaccdfdfb9de6500380145ad6
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-10-08 10:02:20 +0200

    upd: products grid

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,33 +3,33 @@ import "../styles/ProductCard.css";
 import { Link } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
-  if (!product) {
-    return <div>Product not available</div>;
-  }
+	if (!product) {
+		return <div>Product not available</div>;
+	}
 
-  return (
-    <div className="product-card card shadow-sm">
-      <div className="product-card-img-wrapper">
-        <img
-          src={product.image_url}
-          alt={product.name}
-          className="product-card-img card-img-top"
-        />
-      </div>
-      <div className="product-card-body card-body d-flex flex-column">
-        <span className="product-card-title card-title fw-bold">
-          Name: {product.name}
-        </span>
-        <p className="product-card-price card-text">Price: €{product.price}</p>
-        <Link
-          to={`/products/${product.product_id}`}
-          className="btn btn-outline-primary product-card-btn"
-        >
-          Details
-        </Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className="product-card card shadow-sm">
+			<div className="product-card-img-wrapper">
+				<img
+					src={product.image_url}
+					alt={product.name}
+					className="product-card-img card-img-top"
+				/>
+			</div>
+			<div className="product-card-body card-body d-flex flex-column">
+				<span className="product-card-title card-title fw-bold">
+					Name: {product.name}
+				</span>
+				<p className="product-card-price card-text">Price: €{product.price}</p>
+				<Link
+					to={`/products/${product.slug}`}
+					className="btn btn-outline-primary product-card-btn"
+				>
+					Details
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default ProductCard;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -5,74 +5,74 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGripHorizontal } from "@fortawesome/free-solid-svg-icons";
 
 function Products() {
-  const [products, setProducts] = useState([]);
-  const [onClick, setOnClick] = useState(false);
-  const [filterProduct, setFilterProduct] = useState([]);
-  const [search, setSearch] = useState("");
+	const [products, setProducts] = useState([]);
+	const [onClick, setOnClick] = useState(false);
+	const [filterProduct, setFilterProduct] = useState([]);
+	const [search, setSearch] = useState("");
 
-  useEffect(() => {
-    fetch("http://localhost:3000/products")
-      .then((res) => res.json())
-      .then((data) => setProducts(data))
-      .catch((err) => console.error(err));
-  }, []);
+	useEffect(() => {
+		fetch("http://localhost:3000/products")
+			.then((res) => res.json())
+			.then((data) => setProducts(data))
+			.catch((err) => console.error(err));
+	}, []);
 
-  useEffect(() => {
-    const filterProduct = products.filter((product) =>
-      product.name.toLowerCase().includes(search.toLowerCase())
-    );
-    setFilterProduct(filterProduct);
-  }, [search, products]);
+	useEffect(() => {
+		const filterProduct = products.filter((product) =>
+			product.name.toLowerCase().includes(search.toLowerCase())
+		);
+		setFilterProduct(filterProduct);
+	}, [search, products]);
 
-  if (products.length === 0) {
-    return <div className="container loading">Loading...</div>;
-  }
+	if (products.length === 0) {
+		return <div className="container loading">Loading...</div>;
+	}
 
-  return (
-    <div id="products" className="container-fluid hn-main">
-      <div className="row justify-content-center d-flex hn-sections-container">
-        <div className="col-5 my-4 ">
-          <form className="d-flex" role="search">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Find your products"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            <button className="btn btn-outline-success ms-2" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
+	return (
+		<div id="products" className="container-fluid hn-main">
+			<div className="row justify-content-center d-flex hn-sections-container">
+				<div className="col-5 my-4 ">
+					<form className="d-flex" role="search">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Find your products"
+							value={search}
+							onChange={(e) => setSearch(e.target.value)}
+						/>
+						<button className="btn btn-outline-success ms-2" type="submit">
+							Search
+						</button>
+					</form>
+				</div>
 
-        <div className="col-3 my-4 text-white text-center ">
-          <button className="btn" onClick={() => setOnClick(!onClick)}>
-            <FontAwesomeIcon icon={faGripHorizontal} />
-          </button>
-        </div>
-      </div>
+				<div className="col-3 my-4 text-white text-center ">
+					<button className="btn" onClick={() => setOnClick(!onClick)}>
+						<FontAwesomeIcon icon={faGripHorizontal} />
+					</button>
+				</div>
+			</div>
 
-      <div className="row">
-        <div className="col-12 ps-section my-5">
-          <div className="row justify-content-center">
-            {filterProduct.map((product) => (
-              <div
-                className={
-                  !onClick
-                    ? "col-12 col-sm-5 col-md-5 col-lg-2 m-5"
-                    : "col-12 col-sm-5 col-md-5 col-lg-4 m-3"
-                }
-                key={product.product_id}
-              >
-                <ProductCard product={product} />
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+			<div className="row">
+				<div className="col-12 ps-section my-5">
+					<div className="row justify-content-start">
+						{filterProduct.map((product) => (
+							<div
+								className={
+									!onClick
+										? "col-12 col-md-6 col-lg-3 my-2"
+										: "col-12 col-sm-5 col-md-5 col-lg-4 m-3"
+								}
+								key={product.product_id}
+							>
+								<ProductCard product={product} />
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Products;
