a14520c6e8d1c030f6e5f7e5325d9bf22a5a44c2
amletodellaloggia <amletodellaloggia@gmail.com>
2025-10-09 17:13:52 +0200

    🐞 fix:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,51 +6,48 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-dark border-body">
-    <div className="container-fluid">
+  <nav className="navbar navbar-dark bg-dark border-body custom-navbar">
+    <div className="container-fluid custom-navbar-container">
       <NavLink className="navbar-brand" to="/">
         <img src={nerdNestLogo} alt="NerdNest Logo" className="navbar-logo" />
         <span className="brand-text desktop">.nerdNest</span>
         <span className="brand-text mobile">.n</span>
       </NavLink>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <NavLink
-              className={({ isActive }) =>
-                "nav-link" +
-                (isActive ? " nav-active fw-bold" : " fw-semibold")
-              }
-              to="/"
-              end
-            >
-              Home
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              className={({ isActive }) =>
-                "nav-link" + (isActive ? " nav-active fw-bold" : "")
-              }
-              to="/products"
-            >
-              Products
-            </NavLink>
-          </li>
-        </ul>
-        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <NavLink
-              className={({ isActive }) =>
-                "nav-link nav-cart-icon" + (isActive ? " nav-active fw-bold" : "")
-              }
-              to="/cart"
-            >
-              <FontAwesomeIcon icon={faCartShopping} />
-            </NavLink>
-          </li>
-        </ul>
-      </div>
+      <ul className="navbar-nav custom-navbar-nav">
+        <li className="nav-item">
+          <NavLink
+            className={({ isActive }) =>
+              "nav-link" +
+              (isActive ? " nav-active fw-bold" : " fw-semibold")
+            }
+            to="/"
+            end
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            className={({ isActive }) =>
+              "nav-link" + (isActive ? " nav-active fw-bold" : "")
+            }
+            to="/products"
+          >
+            Products
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink
+            className={({ isActive }) =>
+              "nav-link nav-cart-icon" +
+              (isActive ? " nav-active fw-bold" : "")
+            }
+            to="/cart"
+          >
+            <FontAwesomeIcon icon={faCartShopping} />
+          </NavLink>
+        </li>
+      </ul>
     </div>
   </nav>
 );
