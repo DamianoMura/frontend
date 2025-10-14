328258f4cc3be65a5d27b3173039dce9bcbca3f6
CarloMart88 <carlo.martino1988@gmail.com>
2025-10-14 15:55:31 +0200

    add mascotte image and update NotFound page styling

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
+import mascotte from "../assets/imgs/mascotte.webp";
 import "../styles/NotFound.css";
 function NotFound() {
   return (
     <div className="notfound-container">
+      <div className="col-7 mascotte-404 d-flex justify-content-center">
+        <img src={mascotte} alt="Mascotte 404" />
+      </div>
       <h1 className="notfound-title">404 - Page not found</h1>
       <p className="notfound-text">
         We're sorry, but we couldn't find any page
